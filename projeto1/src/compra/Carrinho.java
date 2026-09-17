e33acc0c9182ac9732290741b098e9069b31cfbc
Guilherme Ferraz Cabral <136639704+GuilhermeFCabral@users.noreply.github.com>
2026-09-17 01:16:24 +0000

    edit Carrinho

--- projeto1/src/compra/Carrinho.java
+++ projeto1/src/compra/Carrinho.java
@@ -1,3 +1,38 @@
-public class Carrinho {
+package compra;
+import java.util.ArrayList;
 
+public class Carrinho {
+    private ArrayList<ItemCompra> listaItens;
+    private Double porcentagemDesconto;
+
+    public Carringo(ArrayList itens, Double desc) {
+        listaItens = itens;
+        porcentagemDesconto = desc;
+    }
+
+    public void mostrar() {
+        for (int i=0; i<listaItens.size(); i++) {
+            ListaItens[i].mostrar();
+        }
+
+        System.out.println("Subtotal: "+ obterSubtotal());
+
+    }
+
+    public Double obterSubtotal() {
+        Double subtotal = 0;
+        for (ItemCompra ic : listaItens) {
+            subtotal += ic.obterSubtotal();
+        }
+
+        return subTotal;
+    }
+
+    public Double obterValorDesconto() {
+        return obterSubtotal() * porcentagemDesconto;
+    }
+
+    public Double obterTotal() {
+        return (obterSubtotal() - obterValorDesconto());
+    }
 }
